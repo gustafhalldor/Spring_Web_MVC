684c7d54975ab7b33e6c5461bcad749715c3c6e6
Hoai Nam Duc Tran <aaanarutoaaa@yahoo.com>
2016-10-23 15:57:28 +0000

    yoyo

---
+++ src/main/java/project/persistence/repositories/UserRepository.java
@@ -0,0 +1,41 @@
+package project.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import project.persistence.entities.User;
+
+import java.util.List;
+
+/**
+ * By extending the {@link JpaRepository} we have access to powerful methods.
+ * For detailed information, see:
+ * http://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
+ * http://docs.spring.io/spring-data/data-commons/docs/1.6.1.RELEASE/reference/html/repositories.html
+
+/**
+ * Created by Hoai Nam Duc Tran on 23/10/2016.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User save(User user);
+
+    void delete(User user);
+
+    List<User> findAll();
+
+    // If we need a custom query that maybe doesn't fit the naming convention used by the JPA repository,
+    // then we can write it quite easily with the @Query notation, like you see below.
+    // This method returns all PostitNotes where the length of the name is equal or greater than 3 characters.
+    @Query(value = "SELECT p FROM User p where length(p.name) >= 3 ")
+    List<User> findAllWithNameLongerThan3Chars();
+
+    // Instead of the method findAllReverseOrder() in PostitNoteService.java,
+    // We could have used this method by adding the words
+    // ByOrderByIdDesc, which mean: Order By Id in a Descending order
+    //
+    List<User> findAllByOrderByIdDesc();
+
+    User findOne(Long id);
+
+    List<User> findByName(String name);
+}
